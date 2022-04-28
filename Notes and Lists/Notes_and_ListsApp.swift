557aace75c88551f4d78326b7a10a5e27d50ea8a
Magdiel Borges <magdiel.borges@icloud.com>
2022-04-28 18:15:57 -0400

    Initial Commit

---
+++ Notes and Lists/Notes_and_ListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_and_ListsApp.swift
+//  Notes and Lists
+//
+//  Created by Magdiel Borges on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Notes_and_ListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
